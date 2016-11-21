38db1fc6ef3c53e97e05f2df1debbdad591debbf
worldneo <world.095@outlook.com>
2016-11-21 09:42:56 -0600

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -6,7 +6,7 @@ scrip para calcular la suma de una lista de numeros
 class CalculatorClass:
     
     def _init_(self,lista):
-        self.lista=lista
+        self.lista = lista
     """
     se define la variable lista
     """
@@ -14,7 +14,7 @@ class CalculatorClass:
     def sum(self,lista):
         sum=0
         for i in range(0,len(lista)):
-            sum=sum+lista[i]
+            sum = sum + lista[i]
         return sum
     """
        
