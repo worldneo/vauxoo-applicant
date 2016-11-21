0c862fb7650990b8f856d1057f0e0da05be7c9dc
worldneo <world.095@outlook.com>
2016-11-21 09:53:56 -0600

    Update primes.py

--- primes.py
+++ primes.py
@@ -14,11 +14,11 @@ class PrimeClass(object):
     se instancia el objeto que en este caso es la variable num_int
     """
     def is_prime(self, num_int):
-        divisor=2
-        prim=True
+        divisor = 2
+        prim = True
 
-        while((prim) and (divisor!=num_int)):
-            if (num_int%divisor==0):
+        while((prim) and (divisor != num_int)):
+            if (num_int % divisor == 0):
                 return False
         return True
     """
